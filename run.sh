05b6d1967980e924f34b830c77f1f9627592cfbc
João Rafael <joaoraf@me.com>
2021-05-29 15:02:00 -0300

    remove default env

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 export PYTHONUSERBASE="/home/ftuser/.local"
-set -a; source default.env; set +a
 
 envsubst < user_data/config.live.json > live.json
 envsubst < user_data/config.dry.json > dry.json
