40033b403e043051cafd2d8b78d8dcbc3a5108f6
João Rafael <joaoraf@me.com>
2021-05-29 14:05:07 -0300

    ls bin

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ echo $PYTHONPATH
 # Default config values
 : "${LIVE_STRATEGY:=SMAOffsetV2}"
 : "${DRY_STRATEGY:=CombinedBinHAndClucV8}"
-
+ls /home/ftuser/.local/bin
 envsubst < user_data/config.live.json > live.json
 envsubst < user_data/config.dry.json > dry.json
 
