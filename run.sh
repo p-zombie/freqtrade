c63e50f4166be87da353b2a365916744b4299610
João Rafael <joaoraf@me.com>
2021-05-29 20:33:53 -0300

    disable ui

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ export DRY_DATABASE_URL=${DRY_DATABASE_URL//postgres/postgresql}
 envsubst < user_data/config.live.json > live.json
 envsubst < user_data/config.dry.json > dry.json
 
-freqtrade trade --config live.json --strategy $LIVE_STRATEGY --db-url $LIVE_DATABASE_URL
+honcho start
