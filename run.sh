754e646463de9ae81c5a262722b8385e25667d04
João Rafael <joaoraf@me.com>
2021-05-29 14:11:24 -0300

    get python path

--- run.sh
+++ run.sh
@@ -6,8 +6,11 @@ echo $PYTHONPATH
 # Default config values
 : "${LIVE_STRATEGY:=SMAOffsetV2}"
 : "${DRY_STRATEGY:=CombinedBinHAndClucV8}"
-ls /home/ftuser/.local/bin
+
 envsubst < user_data/config.live.json > live.json
 envsubst < user_data/config.dry.json > dry.json
+
+python -c "import site; print(site.USER_BASE)"
+python -m site --user-base
 
 /home/ftuser/.local/bin/honcho start
