c38e30bfc009afd1aab630713f1540eb7725d409
João Rafael <joaoraf@me.com>
2021-05-29 11:36:32 -0300

    debug run

--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@
 : "${LIVE_STRATEGY:=SMAOffsetV2}"
 : "${DRY_STRATEGY:=CombinedBinHAndClucV8}"
 
+echo "Loading env vars"
 envsubst < user_data/config.live.json > live.json
 envsubst < user_data/config.dry.json > dry.json
 
+ls
 honcho start
