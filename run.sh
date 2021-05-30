c12159e419e6134934455b54523ba26a11962dce
João Rafael <joaoraf@me.com>
2021-05-30 17:18:52 -0300

    change run script

--- run.sh
+++ run.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-export PYTHONUSERBASE="/home/ftuser/.local"
-
-# SQLAlchemy support
-export LIVE_DATABASE_URL=${LIVE_DATABASE_URL//postgres/postgresql}
-export DRY_DATABASE_URL=${DRY_DATABASE_URL//postgres/postgresql}
-
-envsubst < user_data/config.live.json > live.json
-envsubst < user_data/config.dry.json > dry.json
-
+./load_env.sh
 honcho start
