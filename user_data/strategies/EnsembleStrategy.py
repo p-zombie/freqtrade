3a904be81822845df5c51d87c0f409c031b0b245
João Rafael <joaoraf@me.com>
2021-05-31 12:51:10 -0300

    add logging

--- user_data/strategies/EnsembleStrategy.py
+++ user_data/strategies/EnsembleStrategy.py
@@ -1,24 +1,26 @@
 # --- Do not remove these libs ---
 from freqtrade.strategy import IStrategy, IntParameter, DecimalParameter
+import logging
 from pandas import DataFrame
 from freqtrade.resolvers import StrategyResolver
 from itertools import combinations
 from functools import reduce
 import numpy as np
 
+logger = logging.getLogger(__name__)
 np.random.seed(0)
 
 STRATEGIES = [
-    "CombinedBinHAndCluc",
-    "CombinedBinHAndClucV2",
-    "CombinedBinHAndClucV5",
+    #"CombinedBinHAndCluc",
+    #"CombinedBinHAndClucV2",
+    #"CombinedBinHAndClucV5",
     "CombinedBinHAndClucV6H",
     "CombinedBinHAndClucV7",
     "CombinedBinHAndClucV8",
     "CombinedBinHAndClucV8Hyper",
-    "SMAOffset",
+    #"SMAOffset",
     "SMAOffsetV2",
-    "NostalgiaForInfinityV1",
+    #"NostalgiaForInfinityV1",
     "NostalgiaForInfinityV2",
 ]
 
@@ -46,6 +48,13 @@ class EnsembleStrategy(IStrategy):
 
     loaded_strategies = {}
 
+    def __init__(self, config: dict) -> None:
+        super().__init__(config)
+        logger.info(f"Buy stratrategies: {STRAT_COMBINATIONS[self.buy_strategies.value]}")
+        logger.info(f"Buy mean threshold: {self.buy_mean_threshold.value}")
+        logger.info(f"Sell stratrategies: {STRAT_COMBINATIONS[self.sell_strategies.value]}")
+        logger.info(f"Sell mean threshold: {self.sell_mean_threshold.value}")
+
     def get_strategy(self, strategy_name):
         cached_strategy = self.loaded_strategies.get(strategy_name)
         if cached_strategy:
@@ -66,25 +75,26 @@ class EnsembleStrategy(IStrategy):
         for strategy_name in strategies:
             strategy = self.get_strategy(strategy_name)
             strategy_indicators = strategy.advise_indicators(dataframe, metadata)
-            dataframe[f"strat_signal_{strategy_name}"] = strategy.advise_buy(
+            dataframe[f"strat_buy_signal_{strategy_name}"] = strategy.advise_buy(
                 strategy_indicators, metadata
             )["buy"]
 
         dataframe['buy'] = (
-            dataframe.filter(like='strat_signal_').mean(axis=1) > self.buy_mean_threshold.value
+            dataframe.filter(like='strat_buy_signal_').mean(axis=1) > self.buy_mean_threshold.value
         ).astype(int)
         return dataframe
 
     def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         strategies = STRAT_COMBINATIONS[self.sell_strategies.value]
+        strategies = STRATEGIES
         for strategy_name in strategies:
             strategy = self.get_strategy(strategy_name)
             strategy_indicators = strategy.advise_indicators(dataframe, metadata)
-            dataframe[f"strat_signal_{strategy_name}"] = strategy.advise_sell(
+            dataframe[f"strat_sell_signal_{strategy_name}"] = strategy.advise_sell(
                 strategy_indicators, metadata
             )["sell"]
 
         dataframe['sell'] = (
-            dataframe.filter(like='strat_signal_').mean(axis=1) > self.sell_mean_threshold.value
+            dataframe.filter(like='strat_sell_signal_').mean(axis=1) > self.sell_mean_threshold.value
         ).astype(int)
         return dataframe
