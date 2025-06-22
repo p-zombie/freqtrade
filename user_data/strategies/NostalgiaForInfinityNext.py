7297b943f25d570632ab7f3707adee1f7cd34a98
joaorafaelm <joaoraf@me.com>
2025-06-22 15:07:31 +0000

    New release version

--- user_data/strategies/NostalgiaForInfinityNext.py
+++ user_data/strategies/NostalgiaForInfinityNext.py
@@ -1,1 +1,8 @@
-404: Not Found
+
+<html>
+<head>
+<title>Fastly error: unknown domain raw.githubusercontent.com</title>
+</head>
+<body>
+<p>Fastly error: unknown domain: raw.githubusercontent.com. Please check that this domain has been added to a service.</p>
+<p>Details: cache-pao-kpao1770049-PAO (185.199.109.133)</p></body></html>
