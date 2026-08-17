110b0a2e0acf745bbd2a74f25d83d3f89edc0348
joaorafaelm <joaoraf@me.com>
2026-08-17 16:46:57 +0000

    New release version

--- user_data/strategies/NostalgiaForInfinityNext.py
+++ user_data/strategies/NostalgiaForInfinityNext.py
@@ -1,2 +1,1 @@
-429: Too Many Requests
-For more on scraping GitHub and how it may affect your rights, please review our Terms of Service (https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).
+404: Not Found
